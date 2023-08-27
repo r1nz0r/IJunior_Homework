68102cb47302b025840ec9ad4c24363e21a63d75
Tatiana Zaharova <33631903+r1nz0r@users.noreply.github.com>
2023-08-27 19:16:57 +0300

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,16 @@
+namespace IJunior {
+    internal class Program {
+        static void Main(string[] args) {
+            int coinsCollected = 12;
+            double timeForRespawn = 4.8;
+            float armor = 120.8f;
+            bool isDamaged = false;
+            string textAfterDeath = "You dead!";
+            char symbolToContinue = 'Y';
+            long exp = 14000000;
+            byte level = 49;
+            ushort age = 48;
+            uint damage = 408;
+        }
+    }
+}
