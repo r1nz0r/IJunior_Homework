9f1dd1164c365b05cab60959bb93f151130ddea5
Tatiana Zaharova <33631903+r1nz0r@users.noreply.github.com>
2023-08-28 20:41:54 +0300

    Update 6.cs
    1. 60 должна быть переменной. 2. Разделите общее время и минуты на разные переменные.

--- 6.cs
+++ 6.cs
@@ -4,15 +4,16 @@ namespace IJunior {
     internal class Program {
         static void Main(string[] args) {
             int minutesForPatient = 10;
-
+                
             Console.Write("Введите кол-во старушек: ");
             int.TryParse(Console.ReadLine(), out int grannyAmmount);
 
-            int minutesInQueue = grannyAmmount * minutesForPatient;
-            int hoursInQueue = minutesInQueue / 60;
-            minutesInQueue %= 60;
+            int minutesPerHour = 60;
+            int totalMinutesInQueue = grannyAmmount * minutesForPatient;
+            int hoursInQueue = totalMinutesInQueue / minutesPerHour;
+            int remainingMinutesInQueue = totalMinutesInQueue % minutesPerHour;
 
-            Console.WriteLine($"Вы должны отстоять в очереди: часов - {hoursInQueue}, минут - {minutesInQueue}");
+            Console.WriteLine($"Вы должны отстоять в очереди: часов - {hoursInQueue}, минут - {remainingMinutesInQueue}");
         }
     }
 }
