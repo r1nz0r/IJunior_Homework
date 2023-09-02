145d1e3176b37725d3f2641581ce247a3842369f
Tatiana Zaharova <33631903+r1nz0r@users.noreply.github.com>
2023-09-02 19:30:37 +0300

    Update 17.cs

--- 17.cs
+++ 17.cs
@@ -18,26 +18,37 @@ namespace IJunior
             int brackeysCount = 0;
             int maxBrackeySequence = 0;
 
+            int currentOpenBrackeySequence = 0;
+            int currentCloseBrackeySequence = 0;
+
             Console.Write("Введите последовательность скобок: ");
             string brackeys = Console.ReadLine();
 
             foreach (var brackey in brackeys)
-            {
+            {                
+
                 if (brackey == openBrackey)
                 {
                     brackeysCount++;
-                    maxBrackeySequence = 0;
+                    ++currentOpenBrackeySequence;
+                    currentCloseBrackeySequence = 0;
                 }
                 else if (brackey == closeBrackey && brackeysCount > 0)
                 {
                     --brackeysCount;
-                    ++maxBrackeySequence;
+                    ++currentCloseBrackeySequence;
+                    currentOpenBrackeySequence = 0;
                 }
                 else
                 {
                     Console.WriteLine("Последовательность неверная.");
                     return;
                 }
+
+                if (currentOpenBrackeySequence > maxBrackeySequence)
+                    maxBrackeySequence = currentOpenBrackeySequence;
+                else if (currentCloseBrackeySequence > maxBrackeySequence)
+                    maxBrackeySequence = currentCloseBrackeySequence;
             }
 
             if (brackeysCount == 0)
