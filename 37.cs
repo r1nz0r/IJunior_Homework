9ebbd32985f6e3268dff4f84eed37f40e31e550c
Aleksei Boitsov <33631903+r1nz0r@users.noreply.github.com>
2023-09-12 16:02:21 +0300

    Create 37.cs

---
+++ 37.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace IJunior
+{
+    class Program
+    {
+        // Создать класс игрока, с полями, содержащими информацию об игроке и методом, который выводит информацию на экран.
+        // В классе обязательно должен быть конструктор
+
+        private static void Main ()
+        {
+            Player player1 = new Player("пупа", 5);
+            player1.ShowInfo();
+
+            Player player2 = new Player("лупа", 10);
+            player2.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public static int Ids;
+
+        private string _name;
+        private int _id;
+        private int _health;
+
+        static Player ()
+        {
+            Ids = 0;
+        }
+
+        public Player (string name, int health)
+        {
+            _name = name;
+            _health = health;
+            _id = ++Ids;
+        }
+
+        public void ShowInfo ()
+        {
+            Console.WriteLine($"Имя - {_name}, Здоровье - {_health}, ID - {_id}");
+        }
+    }
+}
