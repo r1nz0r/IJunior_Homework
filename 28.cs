b9a8baea2759babba8a4dab3eccf8a5f25325a0d
Aleksei Boitsov <33631903+r1nz0r@users.noreply.github.com>
2023-09-14 18:38:58 +0300

    Update 28.cs
    1. Лекции в большей части показывают, как возможно. Но уходя при этом больше в понимание материала. Практические же задания направлены на то, что бы выработать привычку у вас писать "чистый код". 2. int value = percent / 10; - что здесь значит 10? 3. for (int i = 0; i < value; i++) и for (int i = value; i < maxValue; i++) - циклы одинаковы, стоит выделить метод. И другие повторяющиеся части тоже.

--- 28.cs
+++ 28.cs
@@ -17,7 +17,7 @@ namespace IJunior
             int rage = 40;
             int maxRage = 50;
             int rageBarPoistionX = 0;
-            int rageBarPositionY = 1;        
+            int rageBarPositionY = 1;
 
             int manaPercent = GetPercentFromValue(mana, maxMana);
             int ragePercent = GetPercentFromValue(rage, maxRage);
@@ -31,22 +31,27 @@ namespace IJunior
 
         private static int GetPercentFromValue(int value, int maxValue)
         {
-            double percentFactor = 100;          
+            double percentFactor = 100.0;
             return Convert.ToInt32((double)value / maxValue * percentFactor);
+        }
+
+        private static int GetValueFromPercent(int percent, int maxValue)
+        {
+            double valueFactor = 0.01;
+            return Convert.ToInt32((double)maxValue * valueFactor * percent);
         }
 
         private static void DrawBar(int percent, ConsoleColor color, int positionX, int positionY, char symbol)
         {
             ConsoleColor defaultColor = Console.BackgroundColor;
-            int value = percent / 10;
+
             int maxValue = 10;
+            int value = GetValueFromPercent(percent, maxValue);
 
             var barBuilder = new StringBuilder();
+            int firstIndex = 0;
 
-            for (int i = 0; i < value; i++)
-            {
-                barBuilder.Append(symbol);
-            }
+            FillBar(symbol, firstIndex, value, barBuilder);
 
             string bar = barBuilder.ToString();
 
@@ -58,11 +63,18 @@ namespace IJunior
 
             barBuilder.Clear();
 
-            for (int i = value; i < maxValue; i++)
-                barBuilder.Append(symbol);
+            FillBar(symbol, value, maxValue, barBuilder);
 
             bar = barBuilder.ToString();
             Console.Write(bar + ']');
+        }
+
+        private static void FillBar(char symbol, int minIndex, int maxIndex, StringBuilder barBuilder)
+        {
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                barBuilder.Append(symbol);
+            }
         }
     }
 }
